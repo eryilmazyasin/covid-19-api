938a9090c03a009bf603218141d8e1e911129530
Yasin Eryılmaz <yasineryilmazfb@gmail.com>
2020-04-16 21:42:02 +0300

    first initialize

--- src/App.js
+++ src/App.js
@@ -10,21 +10,22 @@ export default class App extends Component {
     news: null,
     totalData: {},
     turkeyData: {},
-    allData: ''
+    allData: "",
+    instagram: {}
   };
 
   async componentDidMount() {
-    const token = '6ca6PB0jkrJ2Sj99gEKMT6:7IqqvPHXnLYCgY1pW1cbDU';
-    const newsUrl = 'https://api.collectapi.com/corona/coronaNews';
-    const totalDataUrl = 'https://api.collectapi.com/corona/totalData';
-    const countryData = 'https://api.collectapi.com/corona/countriesData';
-    
+    const token = "6ca6PB0jkrJ2Sj99gEKMT6:7IqqvPHXnLYCgY1pW1cbDU";
+    const newsUrl = "https://api.collectapi.com/corona/coronaNews";
+    const totalDataUrl = "https://api.collectapi.com/corona/totalData";
+    const countryData = "https://api.collectapi.com/corona/countriesData";
+   
     //Fetch News
     await axios
       .get(newsUrl, {
         headers: {
-          'authorization': 'apikey ' + token,
-          'content-type': 'application/json'
+          authorization: "apikey " + token,
+          "content-type": "application/json",
         },
       })
       .then((result) => {
@@ -36,13 +37,12 @@ export default class App extends Component {
         console.log(error);
       });
 
-
     //Fetch totalCases
     await axios
       .get(totalDataUrl, {
         headers: {
-          'authorization': 'apikey ' + token,
-          'content-type': 'application/json'
+          authorization: "apikey " + token,
+          "content-type": "application/json",
         },
       })
       .then((result) => {
@@ -54,24 +54,27 @@ export default class App extends Component {
         console.log(error);
       });
 
-      //Fetch Country Data
-      axios.get(countryData, {
+    //Fetch Country Data
+    axios
+      .get(countryData, {
         headers: {
-          'authorization': 'apikey ' + token,
-          'content-type' : 'application/json'
-        }
-      }).then(result => {
-        this.setState({
-          turkeyData : result.data.result[15],
-          allData : result.data.result
-        })
-      }).catch( error=> {
-        console.log(error);
+          authorization: "apikey " + token,
+          "content-type": "application/json",
+        },
       })
+      .then((result) => {
+        this.setState({
+          turkeyData: result.data.result[15],
+          allData: result.data.result,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   render() {
-    const { news, totalData, turkeyData, allData } = this.state;
+    const { news, totalData, turkeyData, allData, instagram } = this.state;
     return (
       <Router>
         <div className="app-container">
@@ -81,10 +84,19 @@ export default class App extends Component {
               path="/"
               exact
               render={(props) => (
-                <Dashboard news={news} totalData={totalData} turkeyData={turkeyData} allData={allData}></Dashboard>
+                <Dashboard
+                  news={news}
+                  totalData={totalData}
+                  turkeyData={turkeyData}
+                  allData={allData}
+                ></Dashboard>
               )}
             ></Route>
-            <Route path="/about" component={About} exact />
+            <Route
+              path="/about"
+              exact
+              render={(props) => <About instagram={instagram}> </About>}
+            ></Route>
             <Route exact component={NotFound}></Route>
           </Switch>
         </div>
@@ -92,5 +104,3 @@ export default class App extends Component {
     );
   }
 }
-
-
--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
 
-export default function About() {
-  return (
-    <section className="about">
-      hakkımda
-    </section>
-  );
+import React, { Component } from 'react'
+
+import axios from "axios"; 
+import User from './User';
+
+export default class About extends Component {
+  state = {
+    value : '',
+    instagram: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+    console.log(this.state.value);
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let val = this.state.value;
+    axios
+    .get(
+     `https://www.instagram.com/${val}/?__a=1&fbclid=IwAR3B5_HaB65jOlr_ixE6C0g_7DdUo0BspGxVsQYULAn1vhXHWqogeDsC5GU`
+    )
+    .then((result) => {
+      this.setState({
+        instagram: result.data.graphql.user,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+
+  }
+
+  render() {
+    return (
+      <div className="container mt-5">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.value} onChange={this.handleChange} className="form-control" placeholder="Fotoğrafını görmek istediğin hesabın kullanıcı adını yaz"/>
+          <input type="submit" value="Görüntüle" className="btn btn-primary mt-2" />
+        </form>
+        <User instagram={this.state.instagram}></User>
+      </div>
+    )
+  }
 }
+
+
+
