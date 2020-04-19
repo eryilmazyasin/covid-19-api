f34d6f3d79b2f7517ce7630f7b0385f60ec5c620
Yasin Eryılmaz <yasineryilmazfb@gmail.com>
2020-04-19 15:32:02 +0300

    about page edited fastly and badly

--- src/components/About.js
+++ src/components/About.js
@@ -1,52 +1,22 @@
-
-import React, { Component } from 'react'
-
-import axios from "axios"; 
-import User from './User';
+import React, { Component } from "react";
+import github from "../images/github.webp";
+import gitlab from "../images/gitlab.png";
 
 export default class About extends Component {
-  state = {
-    value : '',
-    instagram: ''
-  }
-  handleChange = (e) => {
-    this.setState({
-      value: e.target.value
-    })
-    console.log(this.state.value);
-  }
-  handleSubmit = (e) => {
-    e.preventDefault();
-    let val = this.state.value;
-    axios
-    .get(
-     `https://www.instagram.com/${val}/?__a=1&fbclid=IwAR3B5_HaB65jOlr_ixE6C0g_7DdUo0BspGxVsQYULAn1vhXHWqogeDsC5GU`
-    )
-    .then((result) => {
-      this.setState({
-        instagram: result.data.graphql.user,
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-
-
-  }
-
   render() {
     return (
       <div className="container mt-5">
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.value} onChange={this.handleChange} className="form-control" placeholder="Fotoğrafını görmek istediğin hesabın kullanıcı adını yaz"/>
-          <input type="submit" value="Görüntüle" className="btn btn-primary mt-2" />
-        </form>
-        <User instagram={this.state.instagram}></User>
+        <div className="row d-flex justify-content-center">
+          <div className="col-md-4">
+            <img src={github} width="60" />
+            <a href="https:www.github.com/eryilmazyasin" className="ml-3">eryilmazyasin</a>
+          </div>
+          <div className="col-md-4 text-right">
+            <img src={gitlab} width="60" />
+            <a href="https:www.gitlab.com/eryilmazyasin" className="ml-3">eryilmazyasin</a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
-
-
-
- 
